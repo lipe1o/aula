f702383209d516174d619a286f17eb63e7966eba
lipe1o <99926862+lipe1o@users.noreply.github.com>
2022-03-10 18:15:49 -0300

    Add files via upload

---
+++ App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        alunos felipe= new alunos();
+        felipe.setIdade(18);
+        System.out.println(felipe.getIdade());
+   }
+}
---
+++ pessoas.java
@@ -0,0 +1,11 @@
+public class alunos{
+    
+    private int idade;
+
+    public void setIdade(int idade1){
+        this.idade=idade1;
+    }
+    public int getIdade(){
+        return this.idade;
+    }
+}
